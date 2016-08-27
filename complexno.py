cd5e6ecc1be836b7dfa2a76f9a04bc91ee8150ea
rohitrango <rohit.rango@gmail.com>
2016-08-28 01:15:46 +0530

    Compatibility with int+float

--- complexno.py
+++ complexno.py
@@ -1,4 +1,3 @@
-from __future__ import division
 class ComplexNumber: 
 
 	def __init__(self,a=0,b=0):
@@ -16,22 +15,43 @@ class ComplexNumber:
 			return "%.4f + i%.4f"%(self.a,self.b)
 
 	def __add__(self,other):
-		return ComplexNumber(self.a + other.a, self.b+other.b)
+		if isinstance(other,ComplexNumber):
+			return ComplexNumber(self.a + other.a, self.b + other.b)
+		elif isinstance(other,int) or isinstance(other,float):
+			return ComplexNumber(self.a + other, self.b)
+		else:
+			raise TypeError("Other number must be real or complex.")
 
 	def __sub__(self,other):
-		return ComplexNumber(self.a - other.a, self.b-other.b)
+		if isinstance(other,ComplexNumber):
+			return ComplexNumber(self.a - other.a, self.b - other.b)
+		elif isinstance(other,int) or isinstance(other,float):
+			return ComplexNumber(self.a - other, self.b)
+		else:
+			raise TypeError("Other number must be real or complex.")
 
 	def __mul__(self,other):
-		c = (self.a*other.a - self.b*other.b)
-		d = (self.a*other.b + self.b*other.a)
-		return ComplexNumber(c,d)
+		if isinstance(other,ComplexNumber):
+			c = (self.a*other.a - self.b*other.b)
+			d = (self.a*other.b + self.b*other.a)
+			return ComplexNumber(c,d)
+		elif isinstance(other,int) or isinstance(other,float):
+			return ComplexNumber(self.a*other,self.b*other)
+		else:
+			raise TypeError("Other number must be real or complex.")
 
 	def __truediv__(self,other):
-		mod = (other.a)**2 + (other.b)**2
-		if(mod==0):
-			raise AssertionError("Cannot divide by zero.")
-		otherT = ComplexNumber(other.a/mod,-other.b/mod)
-		return self*otherT
+		if isinstance(other,ComplexNumber):
+			mod = (other.a)**2 + (other.b)**2
+			if(mod==0):
+				raise AssertionError("Cannot divide by zero.")
+			otherT = ComplexNumber(other.a/mod,-other.b/mod)
+			return self*otherT
+		elif isinstance(other,int) or isinstance(other,float):
+			if other == 0:
+				raise ArithmeticError("Cannot divide by 0.")
+			else:
+				return ComplexNumber(self.a/other,self.b/other)
 
 	def __eq__(self,other):
 		return (self.a == other.a and self.b == other.b)
